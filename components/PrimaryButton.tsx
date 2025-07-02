6f2f1b0598cac8a0880d84b78edc07af9b55e091
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2025-07-02 16:45:57 -0500

    first layout finished

---
+++ components/PrimaryButton.tsx
@@ -0,0 +1,43 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+type PrimaryButtonProps = {
+  children: string;
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+
+export const PrimaryButton = ({ children }: PrimaryButtonProps) => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        android_ripple={{ color: '#640233' }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
---
+++ screens/StartGameScreen.tsx
@@ -0,0 +1,65 @@
+import { View, TextInput, StyleSheet } from 'react-native';
+import { PrimaryButton } from '../components/PrimaryButton';
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#3b021f',
+    borderRadius: 8,
+    // boxShadow: '2px 0px black',
+    // In previous RN versions, boxShadow was not supported:
+    // android specific:
+    elevation: 100,
+    // iOS specific:
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 'auto',
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
+
+export const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType='number-pad'
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  )
+};
